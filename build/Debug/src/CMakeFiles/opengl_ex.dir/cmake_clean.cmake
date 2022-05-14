6cfc6b945e073bd0449e94963207343d73d0a5d3
antis0007 <31860133+antis0007@users.noreply.github.com>
2022-05-14 13:43:37 -0500

    learnopengl tutorial

---
+++ build/Debug/src/CMakeFiles/opengl_ex.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_ex.dir/gl.c.obj"
+  "CMakeFiles/opengl_ex.dir/main.cpp.obj"
+  "libopengl_ex.dll.a"
+  "opengl_ex.exe"
+  "opengl_ex.exe.manifest"
+  "opengl_ex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/opengl_ex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
